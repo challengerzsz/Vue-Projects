68002aabd47b200b3537764bbffa7ca57d51ffdf
zengshuaizhi <challengerzsz@126.com>
2020-03-14 20:47:56 +0800

    need help

---
+++ Vue-learn-project/src/components/TestAnything.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="login">
+    <span v-bind:title="message">悬停测试</span>
+  </div>
+</template>
+
+<script>
+// var login = new Vue({
+//   el: '#login',
+//   data: {
+//     message: '123456'
+//   }
+// })
+</script>
--- Vue-learn-project/src/router/index.js
+++ Vue-learn-project/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TeestElementUI from '@/components/TestElementUI'
 
 Vue.use(Router)
 
 export default new Router({
+  hashbang: true,
+  history: true,
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'TeestElementUI',
+      component: TeestElementUI
     }
   ]
 })
